a1485d0675ee21c739d034fd606a306b06d496aa
Md Asif <50550167+asifjack@users.noreply.github.com>
2022-02-18 15:25:33 +0530

    Update SQLQuery.sql

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,9 +1,39 @@
-create database student
-use student
-create table sdataa(id int not null identity, sname varchar(50), 
-branch varchar(50), mobile bigint)
-insert into sdataa(sname, branch, mobile) values 
-('ratnesh','dotnet',56464646),
-('shashi','dotnet',56464646),
-('rahul','dotnet',56464646)
-select * from sdataa
+/*create database cmd*/
+create database database_name_asif
+
+/*FOR USING DATABASE*/
+use database_name_asif
+
+
+/* CREATE TABLE CMD */
+CREATE TABLE Persons (
+    PersonID int not null identity,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address_Per varchar(255),
+    City varchar(255)
+);
+
+/* INSERT RECORD IN  TABLE CMD*/
+
+Insert into Persons(LastName,FirstName,Address_Per,City) values
+('asif','md','noida','upnoida');
+
+Insert into Persons values
+('aniket','kumar','new delhi','Delhi');
+
+/* SELECT ALL COLUNM FROM TABLE*/
+Select * from Persons;
+
+Select PersonId,LastName from Persons;
+
+/*update tables data record*/
+update Persons set FirstName='mr' where PersonID=2;
+
+update Persons set Address_per='Bihar' , City='Nalanda' where PersonId=1;
+
+/*DELETE RECORD FROM TABLE*/
+
+
+delete from Persons where City='Nalanda';
+
